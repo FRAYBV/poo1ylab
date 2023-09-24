c242406834a6f0fce48f60ca9b173de64a08d300
FRAYBV <rosas.areli30@gmail.com>
2023-09-24 11:09:02 -0600

    Actividad 1

---
+++ _p01_HolaMundo.java
@@ -0,0 +1,14 @@
+//Envia un saludo a la pantalla
+
+public class _p01_HolaMundo {
+    public static void main(String[] args) {
+        String amigo = "Juan";
+        String hermano = "Pablo";
+        String mensaje = String.format("Tanto %s como %s desean aprender a programar en java", amigo, hermano);
+        
+        System.out.println("Hola Mundo en el Lenguaje Java");
+        System.out.println("\nHola amigo " + amigo + " bienvenido a java");
+        System.out.println("\nMi amigo es " + amigo + " y mi hermano es " + hermano + "\n");
+        System.out.println(mensaje);
+    }
+}
---
+++ _p03_AreaTriangulo.java
@@ -0,0 +1,20 @@
+// Calcula el área de un triángulo
+
+import java.util.Scanner;
+
+public class _p03_AreaTriangulo {
+    public static void main(String[] args) {
+        int base, altura;
+        double area;
+        Scanner obj = new Scanner(System.in);
+
+        System.out.print("Dame la base? ");
+        base = obj.nextInt();
+        System.out.print("Dame la altura? ");
+        altura = obj.nextInt();
+
+        area = base * altura / 2;
+
+        System.out.println(String.format("Un triángulo de base %d y altura %d tiene un área de %f", base, altura, area));
+    }
+}
