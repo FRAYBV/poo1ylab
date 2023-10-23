45654a37252f1d03dfaeafcfbaad9275e2afd077
FRAYBV <rosas.areli30@gmail.com>
2023-10-23 03:07:49 -0600

    Actividad 17

---
+++ _p107_Forma/Forma.java
@@ -0,0 +1,16 @@
+package _p107_Forma;
+
+public abstract class Forma {
+    private String Color;
+
+    public Forma(String color) {
+        Color = color;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        Color = color;
+    }
+    public abstract double getArea();
+}
